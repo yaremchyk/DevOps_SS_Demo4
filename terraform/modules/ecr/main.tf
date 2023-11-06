460235d7d63697f6374304f53fbcfe4e290f26b0
Ruslan <yarema2003r@gmail.com>
2023-11-07 00:02:59 +0200

    ecr module added

---
+++ terraform/modules/ecr/main.tf
@@ -0,0 +1,8 @@
+resource "aws_ecr_repository" "ecr" {
+  name         = "${lower(var.namespace)}/${var.service_name}"
+  force_delete = var.ecr_force_delete
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
