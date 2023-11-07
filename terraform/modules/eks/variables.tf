55df2c5e0a4fecf7c1d37b2f7d4b51bd455a0d67
Ruslan <yarema2003r@gmail.com>
2023-11-07 17:57:15 +0200

    eks edited

--- terraform/modules/eks/variables.tf
+++ terraform/modules/eks/variables.tf
@@ -1,32 +1,50 @@
-variable "eks_cluster_name" {
-  default = "Demo-3"
+variable "public_subnets" {
+  type    = list(string)
+  default = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
+variable "private_subnets" {
+  type    = list(string)
+  default = ["10.0.5.0/24", "10.0.6.0/24"]
+}
+
+variable "logs_namespace" {
+  default     = "aws-observability"
+  description = "Name of the namespace for CloudWatch Logs"
+}
+
+variable "cloud_tech_demo_tags" {
+  description = "Default cloud-tech-demo tags"
+}
+
+variable "ecr_rep" {
+  description = "Name of ECR"
+}
+
+variable "cluster_name" {
+  description = "Name of EKS cluster"
+}
+
+variable "aws_worker_profile" {
+  description = "The AWS default worker profile"
+}
+
+variable "region" {
+  description = "The AWS regions used for our environment"
+}
+
+variable "hosted_zone_name" {
+  description = "Name of hosted zone on Route 53"
+}
+
+variable "alb_name" {
+  description = "Name of ALB"
+}
+
+variable "kms_key_arn" {
+  description = "AWS KMS Key ARN"
 }
 
 variable "vpc" {
   description = "AWS VPC"
 }
-
-variable "public_subnet_cidrs" {
-  type = list(string)
-  default = [
-    "10.0.1.0/24",
-    "10.0.2.0/24"
-  ]
-}
-
-variable "private_subnet_cidrs" {
-  type = list(string)
-  default = [
-    "10.0.3.0/24",
-    "10.0.4.0/24"
-  ]
-}
-
-variable "public_subnet_ids" {
-  type = list(string)
-}
-
-variable "private_subnet_ids" {
-  type = list(string)
-}
-
