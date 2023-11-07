32fecc5e626e32383810e1ff47b3a1936e4af2bb
Ruslan <yarema2003r@gmail.com>
2023-11-07 18:28:57 +0200

    eks edited

--- terraform/modules/eks/variables.tf
+++ terraform/modules/eks/variables.tf
@@ -49,22 +49,6 @@ variable "vpc" {
   description = "AWS VPC"
 }
 
-variable "hosted_zone_name" {
-  description = "Name of hosted zone on Route 53"
-}
-
-variable "alb_name" {
-  description = "Name of ALB"
-}
-
-variable "cloud_tech_demo_tags" {
-  description = "Default cloud-tech-demo tags"
-}
-
-variable "ecr_rep" {
-  description = "Name of ECR"
-}
-
 variable "vpc_name" {
   description = "Name of VPC"
 }
